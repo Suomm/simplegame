e95cdf921b4e7926e30dcd985dc8ebd819a94df0
Suomm <1474983351@qq.com>
2021-03-04 21:48:24 +0800

    点（坐标）

---
+++ src/main/java/com/github/simplegame/support/Point.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 wang shuai, suomm.macher@foxmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.simplegame.support;
+
+import lombok.Data;
+
+import java.util.Random;
+
+/**
+ * <p>
+ * 面板中的坐标。
+ *
+ * <p>
+ * <b>默认以面板的左下角为原点，宽度为x轴，高度为y轴。</b>
+ *
+ * @author 王帅
+ * @since 1.0
+ */
+@Data
+public final class Point {
+
+    /** 横坐标（x轴） */
+    private int x;
+    /** 纵坐标（y轴） */
+    private int y;
+
+    /**
+     * 在面板上随机生成一个位置。
+     *
+     * @param panel 面板
+     */
+    public Point(Panel panel) {
+        Random random = new Random();
+        this.x = random.nextInt(panel.getWidth() + 1);
+        this.y = random.nextInt(panel.getHeight() + 1);
+    }
+
+}
