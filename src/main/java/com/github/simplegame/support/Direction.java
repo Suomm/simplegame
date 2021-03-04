3827b45c79dd80d676b8eebf747f49e92f2430c6
Suomm <1474983351@qq.com>
2021-03-04 21:47:52 +0800

    移动方向枚举

---
+++ src/main/java/com/github/simplegame/support/Direction.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2021 wang shuai, suomm.macher@foxmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.simplegame.support;
+
+import java.util.Random;
+
+/**
+ * 移动方向。
+ *
+ * @author 王帅
+ * @since 1.0
+ */
+public enum Direction {
+
+    /** 上 */
+    UP,
+    /** 下 */
+    DOWN,
+    /** 左 */
+    LEFT,
+    /** 右 */
+    RIGHT;
+
+    private static final Direction[] ENUMS = Direction.values();
+
+    /**
+     * 随机生成一个移动方向。
+     *
+     * @return 移动方向
+     */
+    public static Direction random() {
+        return ENUMS[new Random().nextInt(4)];
+    }
+
+    /**
+     * <p>
+     * 根据索引创建移动方向。
+     *
+     * <p>
+     * 每个索引所表示的方向：
+     * <ul>
+     * <li>0: 上</li>
+     * <li>1: 下</li>
+     * <li>2: 左</li>
+     * <li>3: 右</li>
+     * </ul>
+     *
+     * <p>
+     * <b>如果索引不在这个范围内，则返回{@code null}。</b>
+     *
+     * @param index 索引
+     * @return 移动方向
+     */
+    public static Direction of(int index) {
+        if (index < 0 || index > 3) {
+            return null;
+        }
+        return ENUMS[index];
+    }
+
+}
