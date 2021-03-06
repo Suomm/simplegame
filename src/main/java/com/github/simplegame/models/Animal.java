fbc439f9a1564570c6234229c9060358bd3d18fb
Suomm <1474983351@qq.com>
2021-03-06 17:31:47 +0800

    更新移动规则

--- src/main/java/com/github/simplegame/models/Animal.java
+++ src/main/java/com/github/simplegame/models/Animal.java
@@ -21,6 +21,9 @@ import com.github.simplegame.support.Movable;
 import com.github.simplegame.support.Panel;
 import com.github.simplegame.support.Point;
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
 
 /**
  * 动物模型类，乌龟和鱼的父类。
@@ -29,7 +32,9 @@ import lombok.Data;
  * @since 1.0
  */
 @Data
-public abstract class Animal implements Movable {
+public abstract class Animal implements Movable, Serializable {
+
+    private static final long serialVersionUID = 7428407942268068094L;
 
     /** 动物所在的游戏面板 */
     private final Panel panel;
@@ -42,6 +47,7 @@ public abstract class Animal implements Movable {
      * @param panel 面板
      */
     public Animal(Panel panel) {
+        assert panel != null : "面板不能为空";
         this.panel = panel;
         this.location = new Point(panel);
     }
@@ -52,42 +58,40 @@ public abstract class Animal implements Movable {
      *
      * <p>
      * <b>
-     * 注意：假设物体距离边界还有一步，指定步长为二，则物体会走一步到边界之后再向反方向走去。
+     * 注意：假设物体距离边界还有一步，指定步长为二，则物体会走一步到边界之后不会再移动。
      * </b>
      *
      * @param direction 移动方向
      * @param step 步长
      */
-    public void move(Direction direction, int step) {
+    public void move(@NotNull Direction direction, int step) {
         int temp;
         switch (direction) {
             case UP:
                 temp = location.getY() + step;
                 if (temp > panel.getWidth()) {
-                    step = temp - panel.getWidth();
-                    temp = panel.getWidth() - step;
+                    temp = panel.getWidth();
                 }
                 location.setY(temp);
                 break;
             case DOWN:
                 temp = location.getY() - step;
                 if (temp < 0) {
-                    temp = -temp;
+                    temp = 0;
                 }
                 location.setY(temp);
                 break;
             case LEFT:
                 temp = location.getX() - step;
                 if (temp < 0) {
-                    temp = -temp;
+                    temp = 0;
                 }
                 location.setX(temp);
                 break;
             case RIGHT:
                 temp = location.getX() + step;
                 if (temp > panel.getHeight()) {
-                    step = temp - panel.getHeight();
-                    temp = panel.getHeight() - step;
+                    temp = panel.getHeight();
                 }
                 location.setX(temp);
                 break;
