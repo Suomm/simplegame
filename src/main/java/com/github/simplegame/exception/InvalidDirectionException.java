33a870abdfcf7dfc84c7a35bb630cd8c7e2e0b02
Suomm <1474983351@qq.com>
2021-03-06 17:41:24 +0800

    无效方向异常

---
+++ src/main/java/com/github/simplegame/exception/InvalidDirectionException.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 wang shuai, suomm.macher@foxmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.simplegame.exception;
+
+import org.jetbrains.annotations.Contract;
+
+/**
+ * <p>
+ * 无效的方向异常。这是一个检查时异常，当用户输入的方向索引不满足要求时，抛出该异常。
+ *
+ * @author 王帅
+ * @since 1.1
+ * @see com.github.simplegame.support.Direction
+ */
+public class InvalidDirectionException extends RuntimeException {
+
+    private static final long serialVersionUID = 306027940919150932L;
+
+    /**
+     * 使用详细消息构造异常。
+     *
+     * @param message 详细消息
+     */
+    @Contract(pure = true)
+    public InvalidDirectionException(String message) {
+        super(message);
+    }
+
+}
